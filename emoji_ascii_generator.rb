1c2bb92033297bacf648df917d026025462a7bf8
Yuichi Tateno <hotchpotch@gmail.com>
2020-05-29 18:28:31 +0900

    init

---
+++ emoji_ascii_generator.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+require 'open-uri'
+require 'json'
+
+json = URI.open('https://raw.githubusercontent.com/muan/emojilib/master/emojis.json').read
+# json = open('./emojis.json').read
+emojis = JSON.parse(json)
+
+emoji_list = emojis.map do |main_key, data|
+  [main_key, data['keywords']].flatten.map {|key| [":#{key.upcase}", data['char'], '記号'].join("\t") }
+end.flatten(1).sort.uniq.find_all {|d| d.match(/[a-zA-Z\d]/) }
+
+open('emoji_ascii_dict.txt', 'w') {|file| file.puts emoji_list.join("\n")}
+open('emoji_ascii_dict_u16le.txt', 'w:UTF-16LE') {|file| file.puts emoji_list.join("\r\n")}
